e05c777a63a333a6723d85aec562264667c74738
Dhanashree2023 <pdhanashree88@gmail.com>
2024-04-27 09:43:22 +0530

    first

---
+++ src/pull1com/test1.java
@@ -0,0 +1,12 @@
+package pull1com;
+
+public class test1 {
+	public static void main(String[] args) {
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		System.out.println(4);
+		System.out.println(5);
+	}
+
+}
